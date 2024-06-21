bff2dd1122793d036a055fbf8f68403774bd755d
Valerio <valerio.iachini@prima.it>
2024-06-21 17:10:14 +0200

    clippy

--- disintegrate/src/stream_query.rs
+++ disintegrate/src/stream_query.rs
@@ -59,10 +59,11 @@ impl<E: Clone> StreamQuery<E> {
         }
     }
 
-    pub fn union<U: Event + Clone, O: Clone>(&self, other: &StreamQuery<O>) -> StreamQuery<U>
+    pub fn union<U, O>(&self, other: &StreamQuery<O>) -> StreamQuery<U>
     where
+        U: Event + Clone,
         E: Event + Into<U>,
-        O: Event + Into<U>,
+        O: Event + Into<U> + Clone,
     {
         StreamQuery {
             filter: or(self.filter.clone(), other.filter.clone()),
