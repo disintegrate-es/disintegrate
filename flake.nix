0a6ebc0e4f2dfa8e69d5a025c734915ad0688f30
Valerio Iachini <valerio.iachini@gmail.com>
2026-02-01 14:57:36 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            gcc
+            glibc.dev
+            gnumake
+            pkg-config
+            bison
+            perl
+            openssl
+          ];
+        };
+      }
+    );
+}
